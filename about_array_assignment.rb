543357e4b2060f1988f62b7c3bc28f93ddda2182
Kevin Jing Qiu <kevin@idempotent.ca>
2016-05-16 19:01:36 -0400

    Array assignment

--- about_array_assignment.rb
+++ about_array_assignment.rb
@@ -3,49 +3,49 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class AboutArrayAssignment < Neo::Koan
   def test_non_parallel_assignment
     names = ["John", "Smith"]
-    assert_equal __, names
+    assert_equal ["John", "Smith"], names
   end
 
   def test_parallel_assignments
-    first_name, last_name = ["John", "Smith"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    first_name, last_name = ["John", "Smith"]  # TIL: they call unpacking "parallel assignment"
+    assert_equal "John", first_name
+    assert_equal "Smith", last_name
   end
 
   def test_parallel_assignments_with_extra_values
-    first_name, last_name = ["John", "Smith", "III"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    first_name, last_name = ["John", "Smith", "III"]  # TIL: No tuple size doesn't match exception
+    assert_equal "John", first_name
+    assert_equal "Smith", last_name
   end
 
   def test_parallel_assignments_with_splat_operator
     first_name, *last_name = ["John", "Smith", "III"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "John", first_name
+    assert_equal ["Smith", "III"], last_name
   end
 
   def test_parallel_assignments_with_too_few_variables
-    first_name, last_name = ["Cher"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    first_name, last_name = ["Cher"]  # TIL: Again, no tuple size error (Not enough value to unpack)
+    assert_equal "Cher", first_name
+    assert_equal nil, last_name
   end
 
   def test_parallel_assignments_with_subarrays
     first_name, last_name = [["Willie", "Rae"], "Johnson"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal ["Willie", "Rae"], first_name
+    assert_equal "Johnson", last_name
   end
 
   def test_parallel_assignment_with_one_variable
-    first_name, = ["John", "Smith"]
-    assert_equal __, first_name
+    first_name, = ["John", "Smith"]  # TIL: Again, no need to match the number with the array
+    assert_equal "John", first_name
   end
 
   def test_swapping_with_parallel_assignment
     first_name = "Roy"
     last_name = "Rob"
     first_name, last_name = last_name, first_name
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "Rob", first_name  # TIL: inline swapping, same syntax as python
+    assert_equal "Roy", last_name
   end
 end
